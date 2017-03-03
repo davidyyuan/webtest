c71248cc99f731e8b7cf53f350255584d6ce880f
David Yuan <david.y.yuan@gmail.com>
2017-03-02 22:59:20 -0600

    delete simple app and tests

--- src/main/java/swb/bookstore/infrastructure/mvc/ParamName.java
+++
@@ -1,9 +0,0 @@
-package swb.bookstore.infrastructure.mvc;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ParamName {
-    String value();
-}
